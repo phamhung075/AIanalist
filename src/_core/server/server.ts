eb211819d5782e3951aa9cdd0eac5abacb82191c
Dai Hung PHAM <daihung.pham@gmail.com>
2024-12-21 11:02:44 +0100

    a

--- src/_core/server/server.ts
+++ src/_core/server/server.ts
@@ -1,24 +1,3 @@
-// import express from 'express';
-// import dotenv from 'dotenv';
-// import routes from './routes/routes';  // Correctly import default export from routes.ts
-// import { logRoutes } from '../utils/utils';  // Import utility function for logging routes
-
-// // Load environment variables based on the environment
-// const env = process.env.NODE_ENV || 'development';
-// dotenv.config({ path: `src/environment/.env.${env}` });
-
-// // Initialize Express app
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// // Middleware to use routes
-// app.use(routes);
-
-// // Start the server
-// app.listen(port, () => {
-// 	console.log(`Server running at http://localhost:${port} in ${process.env.NODE_ENV} mode`);
-// 	logRoutes(app); // Log all available routes
-// });
 import { appService} from './app/app.service'
 
 appService.listen().catch(error => console.error('Error starting server:', error));

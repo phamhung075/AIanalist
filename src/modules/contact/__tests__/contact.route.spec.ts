3cb91bc87bb7ca7629c857f59e557ccb048437db
Dai Hung PHAM <daihung.pham@gmail.com>
2024-12-22 23:46:17 +0100

    a

--- src/modules/contact/__tests__/contact.route.spec.ts
+++ src/modules/contact/__tests__/contact.route.spec.ts
@@ -1,6 +1,7 @@
 // __tests__/contact.route.spec.ts
 import { app } from '@/_core/server/app/app.service';
 import request from 'supertest';
+import { IContact } from '../contact.interface';
 
 describe('Contact Routes', () => {
   it('should create a new contact', async () => {
@@ -10,7 +11,7 @@ describe('Contact Routes', () => {
         name: 'Jane Doe',
         email: 'jane@example.com',
         phone: '1234567890',
-      });
+      } as IContact);
 
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty('id');

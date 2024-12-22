245f2f9b0a3db93ef4e7b97c25d0e54a254595a3
Dai Hung PHAM <daihung.pham@gmail.com>
2024-12-22 12:40:15 +0100

    a

--- src/enums/Error.ts
+++
@@ -1,8 +0,0 @@
-export enum EnumErrors {
-	ERROR_UNKNOWN = "Une erreur est survenue.",
-	ERROR_PERMISSION = "Vous n'avez pas les droits pour effectuer cette action.",
-	SESSION_EXPIRED = "La session est expir\u00E9e",
-	NO_USER_LOGGED = "Aucun utilisateur connect\u00E9",
-	INVALID_TOKEN = "Token de session invalide",
-	ERROR_USER_EXIST = "Cet utilisateur existe d\u00E9j\u00E0"
-}

778f17e09ac270e3f1159a40742eb57719874fc2
Dai Hung PHAM <daihung.pham@gmail.com>
2024-12-30 16:26:47 +0100

    ExtendedUserContextRequest

--- src/_core/helper/interfaces/CustomRequest.interface.ts
+++ src/_core/helper/interfaces/CustomRequest.interface.ts
@@ -1,14 +1,15 @@
 import { Request } from "express";
+import { DecodedIdToken } from "firebase-admin/auth";
 
-// Interface pour représenter les détails de l'utilisateur
-export interface UserContext {
-    contactDetails?: any; // Représente les détails du contact récupérés depuis Supabase
-    user?: any;
-}
+// Interface pour représenter les détails de l'utilisateur if not use firebase
+// export interface UserContext {
+//     contactDetails?: any; // Représente les détails du contact récupérés depuis Supabase
+//     user?: any;
+// }
 
 // Étendre la requête Express pour inclure le contexte utilisateur
 export interface ExtendedUserContextRequest extends Request {
-    userContext?: UserContext;
+    user?: DecodedIdToken;
 }
 
 // Étendre la requête pour inclure les paramètres supplémentaires
